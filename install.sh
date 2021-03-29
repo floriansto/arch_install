e33415ca1744191fab2aade4d09b5f9bae27445f
Florian Stockburger <florian.stockburger@web.de>
2021-03-29 23:39:56 +0200

    Add feh, script to set bing image of the day as wallpaper

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ laptop_aur=(libinput-gestures)
 
 desktop_aur=(amdgpu-fan obinskit rtl8814au-aircrack-dkms-git rtl8761b-fw)
 
-user_pkg=(file-roller firefox gimp gparted gpicview libreoffice lm_sensors octave qpdfview speedcrunch thunar thunar-volman thunar-archive-plugin thunderbird vivaldi vlc zip)
+user_pkg=(feh file-roller firefox gimp gparted gpicview libreoffice lm_sensors octave qpdfview speedcrunch thunar thunar-volman thunar-archive-plugin thunderbird vivaldi vlc zip)
 user_aur=(bitwarden-bin nextcloud-client plex-media-player spotify teams zoom)
 
 function error_exit() {
@@ -334,6 +334,9 @@ if [[ $config -eq 1 ]]; then
 else
   sudo -u $user ./dotfiles/install.sh -v standard -g -dw wlp2s0 -de enp4s0
 fi
+
+cd /opt
+git clone https://github.com/markasoftware/bing-wallpaper-linux.git
 
 echo "Set keymap"
 cat <<EOF >/etc/X11/xorg.conf.d/00-keyboard.conf
